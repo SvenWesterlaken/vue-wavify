8a6a95a6336d5609484ba595eb37202807180cab
Sven <sven.westerlaken@hotmail.nl>
2021-01-31 14:35:05 +0100

    sfc init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueWavify from '@/vue-wavify.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueWavify
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-wavify />
+  </div>
+</template>
